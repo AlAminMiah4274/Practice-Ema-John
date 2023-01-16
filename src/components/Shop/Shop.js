98a516b223f890d9524aee1b6e1604656ebc3111
AlAminMiah4274 <alaminmiah4274@gmail.com>
2023-01-16 20:17:35 +0600

    added few css class

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Shop.css';
+
+const Shop = () => {
+    return (
+        <div className='shop-container'>
+            <div className='product-container'>
+                <h2>Product list</h2>
+            </div>
+            <div className='summary-container'>
+                <h2>Order summary</h2>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
