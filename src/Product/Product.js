3568baf7f5f9715d4486583cd0b2be1d078c6966
AlAminMiah4274 <alaminmiah4274@gmail.com>
2023-01-16 21:53:56 +0600

    button adding and design done

--- src/Product/Product.js
+++ src/Product/Product.js
@@ -7,11 +7,14 @@ const Product = (props) => {
         <div className='product'>
             <img src={img} alt="" />
             <div className='product-text'>
-                <p className='product-name'><big>{name}</big></p>
+                <p className='product-name'>{name}</p>
                 <p>Price: ${price}</p>
                 <p><small>Manufacturer: {seller}</small></p>
-                <p><small>Rating: {ratings}</small></p>
+                <p className='product-rating'><small>Rating: {ratings}</small></p>
             </div>
+            <button className='btn-cart'>
+                <p>Add to Cart</p>
+            </button>
         </div>
     );
 };
